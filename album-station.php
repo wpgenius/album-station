49d9d0aeb3e5e095126be42c2e18d4ebd59fe784
sohamjoshi827 <soham@wpgenius.in>
2022-07-04 16:25:44 +0530

    Initial file for plugin

---
+++ album-station.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Album Station
+ * Plugin URI:      https://wpgenius.in/
+ * Description:     Manage portfolio based on album concept using LightGallery.js.
+ * Author:          Team WPGenius
+ * Author URI:      https://wpgenius.in/
+ * Text Domain:     album-station
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Album_Station
+ */
+
+// Your code starts here.
