1033c84a1d7056ed90a33ec2528e9679d694cf52
sohamjoshi827 <soham@wpgenius.in>
2023-06-23 18:15:43 +0530

    Check class exist on page.

--- assets/js/customscript.js
+++ assets/js/customscript.js
@@ -67,13 +67,14 @@ jQuery(document).ready(function(){
     });
 
     jQuery( "input[name='video_choice']:checked" ).trigger( "change" );
-    
-    var jQuerygrid = jQuery('.album-grid').isotope({
-        itemSelector: '.album-station',
-        layoutMode: 'fitRows',
-        percentPosition: true,
-        
-    });
+    if ($(".album-grid").length > 0) {
+		var jQuerygrid = jQuery('.album-grid').isotope({
+			itemSelector: '.album-station',
+			layoutMode: 'fitRows',
+			percentPosition: true,
+
+		});
+	}
     jQuery('.filter-button-group').on( 'click', 'span', function() {
         var filterValue = jQuery(this).attr('data-filter');
         jQuerygrid.isotope({ filter: filterValue });
